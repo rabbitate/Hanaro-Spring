29c937e9be77e748fb588c4324310a338c90ac95
rabbitate <boykhk98@naver.com>
2024-05-02 14:00:15 +0900

    240502

---
+++ jpa/src/main/java/com/hana/JpaApplication.java
@@ -0,0 +1,18 @@
+package com.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+
+}
+
+
+
